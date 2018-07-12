68e12d5eddb354fb448e90825f115299a2b1ec41
sody <xiezhen19920612@163.com>
2018-07-12 16:37:21 +0800

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
+
+// 跨域插件
+var cors = require('cors');
+
 var app = express();
 
+app.use(cors());
 app.use('*', proxy({ target: 'https://www.kuaishouvideo.com', changeOrigin: true }));
 app.listen(6666);
